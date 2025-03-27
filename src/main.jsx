c603965d006b6189ade660d06d3adc044ced3ac1
KalpanaSwami306 <kalpanaswami603@gmail.com>
2025-03-27 09:55:55 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './store.js'
-import { Provider } from 'react-redux'
-import { Toaster } from 'react-hot-toast'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <Provider store={store}>
-       <div className="min-h-screen w-screen overflow-hidden">
-         <App />
-         <Toaster position="top-right"/>
-       </div>
-     </Provider>
-   </StrictMode>
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="min-h-screen w-screen overflow-hidden">
+          <App />
+        </div>
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
